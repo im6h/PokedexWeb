e3b8c6433d6f06e1c6dba6c4e865578492a1f5ee
tjjone98 <tzero1898@gmail.com>
2019-07-19 21:33:44 +0700

    ...

--- src/components/Detail/index.vue
+++ src/components/Detail/index.vue
@@ -1,48 +1,52 @@
 <template>
   <div class="container">
     <app-nav />
-    <div class="jumbotron">
-      <div class="media" v-for="pokemon in getPokemon" :key="pokemon.id">
-        <img class="align-sef-center mr-3" :src="pokemon.img" :alt="pokemon.name" />
-        <div class="media-body">
-          <h5 class="mt-0 text-center">{{pokemon.name}}</h5>
-          <p>Height: {{pokemon.height}}</p>
-          <p>Weith: {{pokemon.weight}}</p>
-          <p>
-            Type :
-            <span v-for="type in pokemon.type" :key="type">[{{type}}]</span>
-          </p>
-          <p>
-            Weaknesses :
-            <span
-              v-for="weaknesses in pokemon.weaknesses"
-              :key="weaknesses"
-            >[{{weaknesses}}]</span>
-          </p>
-          <p>
-            Next evolution :
-            <span
-              v-for="next_evolution in pokemon.next_evolution"
-              :key="next_evolution"
-            >
-              <router-link
-                @click="getPokemon"
-                :to="'/'+next_evolution.num"
-              >[{{next_evolution.name}}]</router-link>
-            </span>
-          </p>
-          <p>
-            Prev evolution :
-            <span
-              v-for="prev_evolution in pokemon.prev_evolution"
-              :key="prev_evolution"
-            >
-              <router-link
-                @click="getPokemon"
-                :to="'/'+prev_evolution.num"
-              >[{{prev_evolution.name}}]</router-link>
-            </span>
-          </p>
+    <div class="container-fluid">
+      <div class="jumbotron">
+        <div class="media" id="item" v-for="pokemon in getPokemon" :key="pokemon.id">
+          <div class="align-self-center mr-3 text-center" :src="pokemon.img" :alt="pokemon.name">
+            <img class="rounded" :src="pokemon.img" :alt="pokemon.name" />
+          </div>
+          <div class="media-body">
+            <h5 class="mt-0 text-center">{{pokemon.name}}</h5>
+            <p>Height: {{pokemon.height}}</p>
+            <p>Weith: {{pokemon.weight}}</p>
+            <p>
+              Type :
+              <span v-for="type in pokemon.type" :key="type">[{{type}}]</span>
+            </p>
+            <p>
+              Weaknesses :
+              <span
+                v-for="weaknesses in pokemon.weaknesses"
+                :key="weaknesses"
+              >[{{weaknesses}}]</span>
+            </p>
+            <p>
+              Next evolution :
+              <span
+                v-for="next_evolution in pokemon.next_evolution"
+                :key="next_evolution"
+              >
+                <router-link
+                  @click="getPokemon"
+                  :to="'/'+next_evolution.num"
+                >[{{next_evolution.name}}]</router-link>
+              </span>
+            </p>
+            <p>
+              Prev evolution :
+              <span
+                v-for="prev_evolution in pokemon.prev_evolution"
+                :key="prev_evolution"
+              >
+                <router-link
+                  @click="getPokemon"
+                  :to="'/'+prev_evolution.num"
+                >[{{prev_evolution.name}}]</router-link>
+              </span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
@@ -81,5 +85,8 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+#item{
+  margin-left: 10%;
+}
 </style>
