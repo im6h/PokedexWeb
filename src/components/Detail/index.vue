856e7ba5b456d3ea20833917b7456fa7a7b94853
tjjone98 <tzero1898@gmail.com>
2019-07-27 22:48:30 +0700

    add mixins

--- src/components/Detail/index.vue
+++ src/components/Detail/index.vue
@@ -77,38 +77,15 @@
 <script>
 import AppNav from "@/components/Header/navbar";
 import AppFooter from "@/components/Footer/footer";
+import {mixins} from "../../mixins/mixin.js";
 export default {
   name: "Detail",
+  mixins:[mixins],
   components: {
     AppNav,
     AppFooter
   },
 
-  data() {
-    return {
-      pokemon: {},
-      type: {
-        Fire: "#FF6144",
-        Water: "#52A9FF",
-        Ghost: "#7D7DC5",
-        Grass: "#8CD46F",
-        Steel: "#B7B7C5",
-        Dragon: "#8C7DF1",
-        Ground: "#E2C56F",
-        Electric: "#FFD452",
-        Dark: "#8C6F61",
-        Normal: "#B7B7A9",
-        Psychic: "#FF6FA9",
-        Bug: "#B7C544",
-        Rock: "#C5B77D",
-        Fighting: "#C56F61",
-        Ice: "#7DD4FF",
-        Flying: "#98A7FF",
-        Poison: "#B76FA9",
-        Fairy: "#F1A9F1"
-      }
-    };
-  },
   computed: {
     getPokemon() {
       return this.$store.getters.pokemon;
---
+++ src/mixins/mixin.js
@@ -0,0 +1,27 @@
+export const mixins = {
+    data() {
+        return {
+            pokemon: {},
+            type: {
+                Fire: "#FF6144",
+                Water: "#52A9FF",
+                Ghost: "#7D7DC5",
+                Grass: "#8CD46F",
+                Steel: "#B7B7C5",
+                Dragon: "#8C7DF1",
+                Ground: "#E2C56F",
+                Electric: "#FFD452",
+                Dark: "#8C6F61",
+                Normal: "#B7B7A9",
+                Psychic: "#FF6FA9",
+                Bug: "#B7C544",
+                Rock: "#C5B77D",
+                Fighting: "#C56F61",
+                Ice: "#7DD4FF",
+                Flying: "#98A7FF",
+                Poison: "#B76FA9",
+                Fairy: "#F1A9F1"
+            }
+        };
+    },
+}
